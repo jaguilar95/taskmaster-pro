09b4702a0cfb23c47e7b530694b557ed741b6e28
Leonardo <josue.aguilar1995@gmail.com>
2022-04-17 20:18:12 -0500

    removed console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,14 +192,7 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function (event, ui) {
-    console.log("drop");
     ui.draggable.remove();
-  },
-  over: function (event, ui) {
-    console.log("over");
-  },
-  out: function (event, ui) {
-    console.log("out");
   },
 });
 
